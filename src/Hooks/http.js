6578fa8a38573978c4602d5814122ab448db55f2
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2020-03-16 17:42:11 -0400

    Specific movie page does not show form unless user is logged in

--- src/Hooks/http.js
+++ src/Hooks/http.js
@@ -4,9 +4,7 @@ export const useHttp = (url, dependencies) => {
 
   const [fetchedData, setFetchedData] = useState([])
 
-  // fetch("http://localhost:3000/movies")
   useEffect(() => {
-    console.log("sending http request");
     fetch(url)
       .then(response => response.json())
       .then(data =>  {
