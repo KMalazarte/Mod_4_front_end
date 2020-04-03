0e5b07cff53087a983fff0b682eb31fca7ccc1cf
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2020-04-03 13:24:58 -0400

    changed user page background and font color and sizing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
@@ -28,7 +28,7 @@ const LoadingIndicator = props => {
 
 ReactDOM.render(
   <>
-    <App />
+    <App/>
     <LoadingIndicator/>
   </>, document.getElementById('root')
 )
